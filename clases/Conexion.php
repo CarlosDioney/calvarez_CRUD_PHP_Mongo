b52ca3442fa6db22e383bf90d6915c0df54b224d
CarlosDioney <a3521110116@alumno.uttehuacan.edu.mx>
2023-03-26 16:03:25 -0600

    primera version

---
+++ clases/Conexion.php
@@ -0,0 +1,30 @@
+<?php
+    require_once $_SERVER['DOCUMENT_ROOT'] . "/CRUD_MONGO/vendor/autoload.php";
+    class Conexion {
+    public function conectar(){
+       try{
+        $servidor = "localhost";
+        $usuario = "mongoadmin";
+        $password = "123456";
+        $baseDatos = "crud";
+        $puerto = "27017";
+
+        $cadenaConexion = "mongodb://" .
+                    $usuario . ":" .
+                    $password . "@" .
+                    $servidor . ":" . 
+                    $puerto . "/" . 
+                    $baseDatos;
+            $cliente = new MongoDB\Client($cadenaConexion);
+            return $cliente->selectDatabase($baseDatos);
+       } catch (\Throwable $th){
+        return $th->getMessage();
+       }
+    }
+}
+
+
+
+
+
+?>
